a1145e1e67c9b91739946df1050421ef06aa1e65
josef-sho <josephshodunke4@gmail.com>
2024-05-28 12:53:50 +0100

    feat: Update polls tab to use FlatList for rendering

--- app/Polls/[id].tsx
+++ app/Polls/[id].tsx
@@ -1,12 +1,46 @@
-import { Stack } from "expo-router";
+import { Stack, useLocalSearchParams} from "expo-router";
 import React from "react";
-import { Text,View } from "react-native";
+import { Text,View,StyleSheet } from "react-native";
 
+
+const Polls = {
+  question:"React native vs Flutter",
+  options:[
+    "React Native FTW",
+    "Flutter is the best",
+    "SwiftUI is better than both",
+  ]
+};
 
 export default function PollsDetails() {
+  const {id} = useLocalSearchParams()
   return (
-    <View>
-        <Text>Details</Text>
+    <View style={styles.container}>
+        <Text style={styles.question}>{Polls.question}</Text>
+        
+        <View style={{gap:5}}>
+        {Polls.options.map((option) => ( 
+          <View key={option} style={styles.optionContainer}>
+            <Text>{option}</Text>
+          </View>
+        ))}
+        </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+
+  container : {
+    padding:10,
+    gap:10
+  },
+  question: {
+    fontSize:20,
+    fontWeight:600,
+  },
+  optionContainer:{
+    backgroundColor:"white",
+    padding:10,
+    borderRadius:5
+  },
+})

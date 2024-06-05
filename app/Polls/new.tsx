2599300a55b0dcfac7505897ee731e472c5247a5
josef-sho <josephshodunke4@gmail.com>
2024-06-05 21:11:05 +0100

    feat: Add header title, input fields for question and options in CreatePoll component

--- app/Polls/new.tsx
+++ app/Polls/new.tsx
@@ -2,6 +2,9 @@ import {View, Text} from "react-native";
 
 export default function CreatePoll() {
   return (
+    //add header title
+    //add input field for question
+    //add input field for options
     <View>
         
     </View>

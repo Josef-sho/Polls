8af737044f1d8e897229b88878f5f328b7ad304e
josef-sho <josephshodunke4@gmail.com>
2024-06-06 23:10:38 +0100

    stuff

--- app/Polls/new.tsx
+++ app/Polls/new.tsx
@@ -3,8 +3,8 @@ import {View, Text} from "react-native";
 export default function CreatePoll() {
   return (
     //add header title
-    //add input field for question
     //add input field for options
+    // 
     <View>
         
     </View>
